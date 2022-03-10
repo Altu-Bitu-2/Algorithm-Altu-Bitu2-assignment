94b3bd40fd5ebc9d70eb29fb72dce525725b1d4b
yyeonzu <tooma22@naver.com>
2022-03-10 17:00:08 +0900

    기존 코드 전역 정렬

--- [정렬] 3월 4일/10804.cpp
+++ [정렬] 3월 4일/10804.cpp
@@ -7,31 +7,37 @@ using namespace std;
 vector<int> arr;
 
 // 카드를 뒤집는 함수
-int cardReverse(){
+int cardReverse()
+{
     int n, m;
 
-    for(int i = 0;i<10;i++){
+    for (int i = 0; i < 10; i++)
+    {
         // 입력
         cin >> n >> m;
-        for(int j = 0;j<(m-n)/2+1;j++){
+        for (int j = 0; j < (m - n) / 2 + 1; j++)
+        {
             // 양끝의 카드부터 순서대로 swap
-            swap(arr[n + j],arr[m - j]);
+            swap(arr[n + j], arr[m - j]);
         }
     }
     return 0;
 }
-int main(){
- 
-    arr.assign(21,0); // 21개의 원소 생성, 0으로 초기화 
+int main()
+{
+
+    arr.assign(21, 0); // 21개의 원소 생성, 0으로 초기화
     // 벡터에 원소 순차적으로 대입
-    for(int i = 0;i<21;i++){
-        arr[i] = i; 
+    for (int i = 0; i < 21; i++)
+    {
+        arr[i] = i;
     }
 
     cardReverse();
-    
+
     //출력
-    for(int i = 1;i<21;i++){ 
+    for (int i = 1; i < 21; i++)
+    {
         cout << arr[i] << ' ';
     }
 
